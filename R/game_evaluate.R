eb60cef0647db4602468840aeee1be36f2bf0ffd
Gopal <gopalpenny@gmail.com>
2020-03-09 14:52:05 -0400

    renamed MM(f)_ to conA(f)_

--- R/game_evaluate.R
+++ R/game_evaluate.R
@@ -40,10 +40,10 @@ evaluate_agreement <- function(params) {
   }
 
   if (aquifer_type == "confined") {
-    q_hat <- MM_qeval(params,MMf_qs0=MM_qshat0,MM_qfhat0,MM_qshat2,MM_qfhat2)
-    q_star <- MM_qeval(params,MMf_qs0=MM_qsstar0,MM_qfstar0,MM_qsstar2,MM_qfstar2)
-    q_double <- MM_qeval(params %>% dplyr::mutate(qshat=q_hat$qs,qfhat=q_hat$qf),
-                         MMf_qs0=MM_qsdouble0,MM_qfdouble0,MM_qsdouble2,MM_qfdouble2)
+    q_hat <- conA_qeval(params,conAf_qs0=conA_qshat0,conA_qfhat0,conA_qshat2,conA_qfhat2)
+    q_star <- conA_qeval(params,conAf_qs0=conA_qsstar0,conA_qfstar0,conA_qsstar2,conA_qfstar2)
+    q_double <- conA_qeval(params %>% dplyr::mutate(qshat=q_hat$qs,qfhat=q_hat$qf),
+                         conAf_qs0=conA_qsdouble0,conA_qfdouble0,conA_qsdouble2,conA_qfdouble2)
   } else {
     first_best <- with(params,function(x) {
       F1 <- p0s+1/2*B*(-((Dfs*x[2])/sqrt(d0f-rmT*rsT+Dfs*x[1]+Dff*x[2]))+(-2*d0s+2*rmT*rsT-3*Dss*x[1]-2*Dsf*x[2])/sqrt(d0s-rmT*rsT+Dss*x[1]+Dsf*x[2]))
@@ -58,8 +58,8 @@ evaluate_agreement <- function(params) {
                    qsdouble=q_double$qs,qfdouble=q_double$qf)
 
   # get z constraints
-  zMaxFrench_calc <- MM_zMaxFrench(params,q_vals)
-  zMinSwiss_calc <- MM_zMinSwiss(params,q_vals)
+  zMaxFrench_calc <- conA_zMaxFrench(params,q_vals)
+  zMinSwiss_calc <- conA_zMinSwiss(params,q_vals)
   zRange_calc <- zMaxFrench_calc - zMinSwiss_calc
   treaty <- ifelse(zRange_calc>0,"Y","N")
   return(tibble::tibble(treaty=treaty,zRange=zRange_calc,
@@ -77,14 +77,14 @@ evaluate_agreement_utility <- function(params,q_vals) {
   }
   for (v in 1:dim(q_vals)[2]) {assign(names(q_vals)[v], q_vals[[v]])}
   # get utilities
-  Us_hat <- MM_Us(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
-  Uf_hat <- MM_Uf(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
-  Us_star <- MM_Us(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rs=rsN),z=0)
-  Uf_star <- MM_Uf(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rf=rfN),z=0)
-  Us_double <- MM_Us(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
-  Uf_double <- MM_Uf(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
-  Us_hat_double <- MM_Us(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
-  Uf_hat_double <- MM_Uf(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
+  Us_hat <- conA_Us(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
+  Uf_hat <- conA_Uf(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
+  Us_star <- conA_Us(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rs=rsN),z=0)
+  Uf_star <- conA_Uf(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rf=rfN),z=0)
+  Us_double <- conA_Us(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
+  Uf_double <- conA_Uf(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
+  Us_hat_double <- conA_Us(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rs=rsT),z=0)
+  Uf_hat_double <- conA_Uf(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT),z=0)
   u_vals <- tibble(Us_hat=Us_hat,Uf_hat=Uf_hat,
                    Us_star=Us_star,Uf_star=Uf_star,
                    Us_double=Us_double,Uf_double=Uf_double,
@@ -102,14 +102,14 @@ evaluate_agreement_depths <- function(params,q_vals) {
   }
   for (v in 1:dim(q_vals)[2]) {assign(names(q_vals)[v], q_vals[[v]])}
   # get depths
-  ds_hat <- MM_ds(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
-  df_hat <- MM_df(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT))
-  ds_star <- MM_ds(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rs=rsN))
-  df_star <- MM_df(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rf=rfN))
-  ds_double <- MM_ds(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
-  df_double <- MM_df(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT))
-  ds_hat_double <- MM_ds(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
-  df_hat_double <- MM_df(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT))
+  ds_hat <- conA_ds(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
+  df_hat <- conA_df(qs=qshat,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rf=rfT))
+  ds_star <- conA_ds(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rs=rsN))
+  df_star <- conA_df(qs=qsstar,qf=qfstar,params %>% dplyr::mutate(rm=rmN,rf=rfN))
+  ds_double <- conA_ds(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
+  df_double <- conA_df(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT))
+  ds_hat_double <- conA_ds(qs=qsdouble,qf=qfhat,params %>% dplyr::mutate(rm=rmT,rs=rsT))
+  df_hat_double <- conA_df(qs=qshat,qf=qfdouble,params %>% dplyr::mutate(rm=rmT,rf=rfT))
   d_vals <- tibble(ds_hat=ds_hat,df_hat=df_hat,
                    ds_star=ds_star,df_star=df_star,
                    ds_double=ds_double,df_double=df_double,
@@ -121,7 +121,7 @@ evaluate_agreement_depths <- function(params,q_vals) {
 #' Evaluate multiple treaty scenarios
 #'
 #' Evaluate whether or not the treaty will be made under multiple scenarios
-#' @param params Data.frame of parameters with each row sent to \code{evaluate_agreement()}.
+#' @param params_df Data.frame of parameters with each row sent to \code{evaluate_agreement()}.
 #' @param return_criteria Character string containing letters that indicate output variables. See details.
 #' @details
 #' Evaluate the treaty given multiple combinations of social, economic,
@@ -129,17 +129,25 @@ evaluate_agreement_depths <- function(params,q_vals) {
 #' This function takes a data.frame of parameters,
 #' evaluates each row to see if a treaty is signed,
 #' and returns a tibble with the results and original params.
-#' \enumerate{return_criteria}
-#' \item{q - will return abstraction rates}
-#' \item{p - will return only parameters different from default. Otherwise all parameters returned}
-#' \item{a - returns all parameters (i.e., it's redundant to include a AND p}
+#'
+#' The parameter \code{return_criteria} can contain the following letters:
+#' \itemize{
+#' \item q - will return abstraction rates
+#' \item p - will return only parameters different from default. Otherwise all parameters returned
+#' \item a - return all parameters (i.e., it's redundant to include a AND p)
+#' \item u - return utilities of each player
+#' \item d - return depth to water table for each player
+#' }
 #' @return
-#' Returns a 1-row tibble containing pumping, utility ranges needed for the treaty,
-#' and whether or not there is a treaty (i.e., if zRange > 0)
+#' Returns a \code{tibble} containing values utility ranges needed for the treaty,
+#' and whether or not there is a treaty (i.e., if zRange > 0), as well as output specified by \code{return_criteria}.
 #' @importFrom magrittr %>%
 #' @export
 #' @examples
-#' evaluate_agreement(default_params)
+#' library(genevoisgame)
+#' params_df <- genevoisgame::default_params
+#' evaluate_agreement_cases(df)
+#' evaluate_agreement_cases(default_params,'')
 evaluate_agreement_cases <- function(params_df,return_criteria="qp") {
   params_list <- split(params_df,1:dim(params_df)[1])
   eval_results <- do.call(rbind,lapply(params_list,evaluate_agreement))
@@ -148,12 +156,11 @@ evaluate_agreement_cases <- function(params_df,return_criteria="qp") {
   eval_return <- eval_results %>% dplyr::select(treaty,zRange,zMinSwiss,zMaxFrench)
   if (max(grepl("p",return_criteria))==1) { # identify parameters that do not vary AND are equal to default value
     vars_stable <- params_df %>% tidyr::gather(variable,value) %>% dplyr::group_by(variable) %>%
-      dplyr::summarize(value_mean=mean(value),
-                value_max=max(value)) %>%
-      dplyr::left_join(params_default() %>% tidyr::gather(variable,default),by="variable") %>%
-      dplyr::filter(value_mean==value_max,value_mean==default) %>%
+      dplyr::summarize(n_unique=length(unique(value))) %>%
+      # dplyr::left_join(params_default() %>% tidyr::gather(variable,default),by="variable") %>%
+      dplyr::filter(n_unique==1) %>%
       dplyr::pull(variable)
-    params_cases <- params_df %>% dplyr::select(-dplyr::match(vars_stable,names(params_df))) # remove identified parameters
+    params_cases <- params_df %>% dplyr::select(-match(vars_stable,names(params_df))) # remove identified parameters
     eval_return <- eval_return %>% dplyr::bind_cols(params_cases)
   }
   if (max(grepl("a",return_criteria))==1) { # return all parameters
--- R/game_functions_confined.R
+++ R/game_functions_confined.R
@@ -15,12 +15,11 @@ apply_constraints <- function(x,interval) {
   return(x)
 }
 
-MM <- list()
-#
+
 # 4a function to evaluate abstraction rates
-MM_qeval <- function(params,MMf_qs0,MMf_qf0,MMf_qs2,MMf_qf2) {
-  qs0 <- MMf_qs0(params) # get initial q_hat
-  qf0 <- MMf_qf0(params)
+conA_qeval <- function(params,conAf_qs0,conAf_qf0,conAf_qs2,conAf_qf2) {
+  qs0 <- conAf_qs0(params) # get initial q_hat
+  qf0 <- conAf_qf0(params)
   qs1 <- qs0
   qf1 <- qf0
   qs2 <- apply_constraints(qs1,c(0,params$Qs)) # contrain q_hat to bounds
@@ -30,8 +29,8 @@ MM_qeval <- function(params,MMf_qs0,MMf_qf0,MMf_qs2,MMf_qf2) {
       # cat('qs1 =',qs1,'\nqs2 =',qs2,'\nqf1 =',qf1,'\nqf2 =',qf2,"\n")
       qs1 <- qs2 # store old
       qf1 <- qf2
-      qs2 <- apply_constraints(MMf_qs2(params,qf1),c(0,params$Qs)) # update qshat with constrained qfhat
-      qf2 <- apply_constraints(MMf_qf2(params,qs1),c(0,params$Qf)) # update qfhat with constrained qshat
+      qs2 <- apply_constraints(conAf_qs2(params,qf1),c(0,params$Qs)) # update qshat with constrained qfhat
+      qf2 <- apply_constraints(conAf_qf2(params,qs1),c(0,params$Qf)) # update qfhat with constrained qshat
     } else {
       return(list(qs=qs2,qf=qf2))
     }
@@ -43,88 +42,87 @@ MM_qeval <- function(params,MMf_qs0,MMf_qf0,MMf_qs2,MMf_qf2) {
 
 # 4b Mathematica functions for depth, utility, and abstraction
 
-MM_Us <- function(qs,qf,params,z) {
+conA_Us <- function(qs,qf,params,z) {
   with(params,
        -es-p0s*(-qs+Qs)-crs*rm-B*qs*(d0s+Dsf*qf+Dss*qs-rm*rs)+z
   )}
-MM_Uf <- function(qs,qf,params,z) {
+conA_Uf <- function(qs,qf,params,z) {
   with(params,
        -ef-p0f*(-qf+Qf)-B*qf*(d0f+Dff*qf+Dfs*qs-rf*rm)-z
   )}
-
-MM_ds <- function(qs,qf,params) {
+conA_ds <- function(qs,qf,params) {
   with(params,
        d0s+Dsf*qf+Dss*qs-rm*rs
   )}
-MM_df <- function(qs,qf,params) {
+conA_df <- function(qs,qf,params) {
   with(params,
        d0f+Dff*qf+Dfs*qs-rf*rm
   )}
 
-MM_qshat0 <- function(params) {
+conA_qshat0 <- function(params) {
   with(params,
        (Dfs*p0f+Dsf*p0f-2*Dff*p0s+B*(2*d0s*Dff-d0f*(Dfs+Dsf)+rmT*(Dfs*rfT+Dsf*rfT-2*Dff*rsT)))/(B*(Dfs^2+2*Dfs*Dsf+Dsf^2-4*Dff*Dss))
   )}
-MM_qfhat0 <- function(params) {
+conA_qfhat0 <- function(params) {
   with(params,
        (-2*Dss*p0f+(Dfs+Dsf)*p0s+B*(-d0s*(Dfs+Dsf)+2*d0f*Dss-2*Dss*rfT*rmT+(Dfs+Dsf)*rmT*rsT))/(B*(Dfs^2+2*Dfs*Dsf+Dsf^2-4*Dff*Dss))
   )}
-MM_qshat2 <- function(params,qf1) {
+conA_qshat2 <- function(params,qf1) {
   with(params,
        (p0s-B*(d0s+Dfs*qf1+Dsf*qf1-rmT*rsT))/(2*B*Dss)
   )}
-MM_qfhat2 <- function(params,qs1) {
+conA_qfhat2 <- function(params,qs1) {
   with(params,
        (p0f-B*(d0f+Dfs*qs1+Dsf*qs1-rfT*rmT))/(2*B*Dff)
   )}
 # qi_star
-MM_qsstar0 <- function(params) {
+conA_qsstar0 <- function(params) {
   with(params,
        (2*B*d0s*Dff-B*d0f*Dsf+Dsf*p0f-2*Dff*p0s+B*Dsf*rfN*rmN-2*B*Dff*rmN*rsN)/(B*Dfs*Dsf-4*B*Dff*Dss)
   )}
-MM_qfstar0 <- function(params) {
+conA_qfstar0 <- function(params) {
   with(params,
        (-2*Dss*p0f+Dfs*p0s+B*(-d0s*Dfs+2*d0f*Dss-2*Dss*rfN*rmN+Dfs*rmN*rsN))/(B*(Dfs*Dsf-4*Dff*Dss))
   )}
-MM_qsstar2 <- function(params,qf1) {
+conA_qsstar2 <- function(params,qf1) {
   with(params,
        (p0s-B*(d0s+Dsf*qf1-rmN*rsN))/(2*B*Dss)
   )}
-MM_qfstar2 <- function(params,qs1) {
+conA_qfstar2 <- function(params,qs1) {
   with(params,
        (p0f-B*(d0f+Dfs*qs1-rfN*rmN))/(2*B*Dff)
   )}
 # qi_double
-MM_qsdouble0 <- function(params) { # in call: include params %>% mutate(qfhat=qfhat,qshat=qshat)
+conA_qsdouble0 <- function(params) { # in call: include params %>% mutate(qfhat=qfhat,qshat=qshat)
   with(params,
        (Dsf*(p0f-gs*p0f)-2*Dff*p0s+B*(2*d0s*Dff+d0f*Dsf*(-1+gs)+2*Dff*Dsf*gs*qfhat-Dfs*Dsf*gf*qshat+Dfs*Dsf*gf*gs*qshat+Dsf*rfT*rmT-Dsf*gs*rfT*rmT-2*Dff*rmT*rsT))/(B*(-4*Dff*Dss+Dfs*Dsf*(-1+gf)*(-1+gs)))
   )}
-MM_qfdouble0 <- function(params) { # in call: include params %>% mutate(qfhat=qfhat,qshat=qshat)
+conA_qfdouble0 <- function(params) { # in call: include params %>% mutate(qfhat=qfhat,qshat=qshat)
   with(params,
        (-2*Dss*p0f-Dfs*(-1+gf)*p0s+B*(2*d0f*Dss+d0s*Dfs*(-1+gf)-Dfs*Dsf*gs*qfhat+Dfs*Dsf*gf*gs*qfhat+2*Dfs*Dss*gf*qshat-2*Dss*rfT*rmT+Dfs*rmT*rsT-Dfs*gf*rmT*rsT))/(B*(-4*Dff*Dss+Dfs*Dsf*(-1+gf)*(-1+gs)))
   )}
-MM_qsdouble2 <- function(params,qf1) {
+conA_qsdouble2 <- function(params,qf1) {
   with(params,
        (p0s-B*(d0s+Dsf*(qf1-gs*qf1+gs*qfhat)-rmT*rsT))/(2*B*Dss)
   )}
-MM_qfdouble2 <- function(params,qs1) {
+conA_qfdouble2 <- function(params,qs1) {
   with(params,
        (p0f-B*(d0f+Dfs*(qs1-gf*qs1+gf*qshat)-rfT*rmT))/(2*B*Dff)
   )}
 
 # 4c Mathematica functions for z constraints
 # z constraints
-MM_zMinSwiss <- function(params,q_vals) {
+conA_zMinSwiss <- function(params,q_vals) {
   with(q_vals, # q_vals should include qsstar,qfstar,qshat,qfhat,qsdouble,qfdouble
        with(params,
             es+p0s*(Qs-qshat)-p0s*(Qs-qsstar)-crs*rmN+crs*rmT-B*qsstar*(d0s+Dsf*qfstar+Dss*qsstar-rmN*rsN)+B*qshat*(d0s+Dsf*qfdouble+Dss*qshat-rmT*rsT)-gs*(B*qshat*(d0s+Dsf*qfdouble+Dss*qshat-rmT*rsT)-B*qshat*(d0s+Dsf*qfhat+Dss*qshat-rmT*rsT))
        ))}
-MM_zMaxFrench <- function(params,q_vals) {
+conA_zMaxFrench <- function(params,q_vals) {
   with(q_vals,
        with(params,
             -ef-p0f*(Qf-qfhat)+p0f*(Qf-qfstar)+B*qfstar*(d0f+Dff*qfstar+Dfs*qsstar-rfN*rmN)-B*qfhat*(d0f+Dff*qfhat+Dfs*qsdouble-rfT*rmT)+gf*(B*qfhat*(d0f+Dff*qfhat+Dfs*qsdouble-rfT*rmT)-B*qfhat*(d0f+Dff*qfhat+Dfs*qshat-rfT*rmT))
        ))}
-MM_zRange <- function(params,q_vals) {
-  zRange <- MM_zMaxFrench(params,q_vals) - MM_zMinSwiss(params,q_vals)
+conA_zRange <- function(params,q_vals) {
+  zRange <- conA_zMaxFrench(params,q_vals) - conA_zMinSwiss(params,q_vals)
   return(zRange)
 }
