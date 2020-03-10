5ffd01e098b8b1eb42f9eb6a3820b329fcdae696
Gopal <gopalpenny@gmail.com>
2020-03-10 15:26:37 -0400

    added test for single game

--- tests/testthat/test-game-evaluate.R
+++ tests/testthat/test-game-evaluate.R
@@ -3,3 +3,19 @@
 test_that("evaluate_agreement returns tibble",{
   expect_equal(class(evaluate_treaty(default_params)),c("tbl_df","tbl","data.frame"))
 })
+
+df <- tibble::tibble(treaty="Y",
+                 zRange=0.044324885034313,
+                 zMinSwiss=-0.0221624425171565,
+                 zMaxFrench=0.0221624425171565,
+                 qshat=6.86857142857143,
+                 qfhat=6.86857142857143,
+                 qsstar=8.74181818181818,
+                 qfstar=8.74181818181818,
+                 qsdouble=9.0375939849624,
+                 qfdouble=9.0375939849624) %>%
+  dplyr::mutate_if(is.numeric,function(x) round(x,digits=4))
+test_that("evaluate_agreement returns correct zRange, q values",{
+  expect_equal(evaluate_treaty(default_params) %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)),
+               df)
+})
