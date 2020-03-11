0e2855cb9206791ed02fd6a140abd89ec76da232
Gopal <gopalpenny@gmail.com>
2020-03-10 20:02:10 -0400

    reorganized how q functions are defined

--- R/game_functions_unconfined.R
+++ R/game_functions_unconfined.R
@@ -27,42 +27,22 @@ evaluate_treaty_unconfined <- function(params) {
   params$phi0s <- params$h0s^2
   params$phi0f <- params$h0f^2
 
-  # get roots for F1 = F2 = 0, solving for Qs, Qf
-  first_best_equations <- with(params,function(x) {
-    F1<--Bs*dBs+p0s-(Bf*PHIfs*x[2])/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])+(Bs*(2*phi0s+2*PHIrs*rm-3*PHIss*x[1]-2*PHIsf*x[2]))/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
-    F2<--Bf*dBf+p0f+(Bf*(2*phi0f+2*PHIrf*rm-2*PHIfs*x[1]-3*PHIff*x[2]))/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])-(Bs*PHIsf*x[1])/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
-    return(list(F1=F1,F2=F2))
-  })
-  q_hat <- rootSolve::multiroot(f=first_best_equations,start=c(params$Qs*0.9,params$Qf*0.9))
-
-  # get roots for N1 = N2 = 0, solving for Qs, Qf
-  nash_equations <- with(params,function(x) {
-    N1<--Bs*dBs+p0s+(Bs*(2*phi0s+2*PHIrs*rm-3*PHIss*x[1]-2*PHIsf*x[2]))/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
-    N2<--Bf*dBf+p0f+(Bf*(2*phi0f+2*PHIrf*rm-2*PHIfs*x[1]-3*PHIff*x[2]))/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])
-    return(list(N1=N1, N2=N2))
-  })
-  q_star <- rootSolve::multiroot(f=nash_equations,start=c(params$Qs*0.9,params$Qf*0.9))
-
-  # get roots for D1 = D2 = 0, solving for Qs, Qf
-  cheat_equations <- with(params,function(x) {
-    D1<-gs*(p0s-(Bs*PHIss*x[1])/(2*sqrt[phi0s-PHIsf*qfhat+PHIrs*rmT-PHIss*x[1]])-Bs*(dBs-sqrt[phi0s-PHIsf*qfhat+PHIrs*rmT-PHIss*x[1]]))+(1-gs)*(p0s-(Bs*PHIss*x[1])/(2*sqrt[phi0s+PHIrs*rmT-PHIss*x[1]-PHIsf*x[2]])-Bs*(dBs-sqrt[phi0s+PHIrs*rmT-PHIss*x[1]-PHIsf*x[2]]))
-    D2<-gf*(p0f-(Bf*PHIff*x[2])/(2*sqrt[phi0f-PHIfs*qshat+PHIrf*rmT-PHIff*x[2]])-Bf*(dBf-sqrt[phi0f-PHIfs*qshat+PHIrf*rmT-PHIff*x[2]]))+(1-gf)*(p0f-(Bf*PHIff*x[2])/(2*sqrt[phi0f+PHIrf*rmT-PHIfs*x[1]-PHIff*x[2]])-Bf*(dBf-sqrt[phi0f+PHIrf*rmT-PHIfs*x[1]-PHIff*x[2]]))
-    return(list(D1=D1, D2=D2))
-  })
-  q_double <- rootSolve::multiroot(f=cheat_equations,start=c(params$Qs*0.9,params$Qf*0.9))
+  q_hat <- unconA_qhat(params)
+  q_star <- unconA_qstar(params)
+  q_double <- unconA_qdouble(params)
 
   q_vals <- tibble::tibble(qshat=q_hat[1],qfhat=q_hat[2],
                            qsstar=q_star[1],qfstar=q_star[2],
                            qsdouble=q_double[1],qfdouble=q_double[2])
-  #
+
   # # get z constraints
-  # zMaxFrench_calc <- conA_zMaxFrench(params,q_vals)
-  # zMinSwiss_calc <- conA_zMinSwiss(params,q_vals)
-  # zRange_calc <- zMaxFrench_calc - zMinSwiss_calc
-  # treaty <- ifelse(zRange_calc>0,"Y","N")
-  # return(tibble::tibble(treaty=treaty,zRange=zRange_calc,
-  #               zMinSwiss=zMinSwiss_calc,zMaxFrench=zMaxFrench_calc) %>%
-  #          dplyr::bind_cols(q_vals))
+  zMaxFrench_calc <- unconA_zMaxFrench(params,q_vals)
+  zMinSwiss_calc <- unconA_zMinSwiss(params,q_vals)
+  zRange_calc <- zMaxFrench_calc - zMinSwiss_calc
+  treaty <- ifelse(zRange_calc>0,"Y","N")
+  return(tibble::tibble(treaty=treaty,zRange=zRange_calc,
+                zMinSwiss=zMinSwiss_calc,zMaxFrench=zMaxFrench_calc) %>%
+           dplyr::bind_cols(q_vals))
 }
 
 
@@ -84,3 +64,55 @@ unconA_df <- function(qs,qf,params) {
   with(params,
        dBf-sqrt[h0f^2-PHIff*qf-PHIfs*qs+PHIrf*rm]
   )}
+
+# q values
+
+unconA_qhat <- function(params) {
+  # get roots for F1 = F2 = 0, solving for Qs, Qf
+  first_best_equations <- with(params,function(x) {
+    F1<--Bs*dBs+p0s-(Bf*PHIfs*x[2])/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])+(Bs*(2*phi0s+2*PHIrs*rm-3*PHIss*x[1]-2*PHIsf*x[2]))/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
+    F2<--Bf*dBf+p0f+(Bf*(2*phi0f+2*PHIrf*rm-2*PHIfs*x[1]-3*PHIff*x[2]))/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])-(Bs*PHIsf*x[1])/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
+    return(list(F1=F1,F2=F2))
+  })
+  q_hat <- rootSolve::multiroot(f=first_best_equations,start=c(params$Qs*0.9,params$Qf*0.9))
+  return(q_hat)
+}
+
+unconA_qstar <- function(params) {
+  # get roots for N1 = N2 = 0, solving for Qs, Qf
+  nash_equations <- with(params,function(x) {
+    N1<--Bs*dBs+p0s+(Bs*(2*phi0s+2*PHIrs*rm-3*PHIss*x[1]-2*PHIsf*x[2]))/(2*sqrt[phi0s+PHIrs*rm-PHIss*x[1]-PHIsf*x[2]])
+    N2<--Bf*dBf+p0f+(Bf*(2*phi0f+2*PHIrf*rm-2*PHIfs*x[1]-3*PHIff*x[2]))/(2*sqrt[phi0f+PHIrf*rm-PHIfs*x[1]-PHIff*x[2]])
+    return(list(N1=N1, N2=N2))
+  })
+  q_star <- rootSolve::multiroot(f=nash_equations,start=c(params$Qs*0.9,params$Qf*0.9))
+  return(q_star)
+}
+
+unconA_qdouble <- function(params) {
+  # get roots for D1 = D2 = 0, solving for Qs, Qf
+  cheat_equations <- with(params,function(x) {
+    D1<-gs*(p0s-(Bs*PHIss*x[1])/(2*sqrt[phi0s-PHIsf*qfhat+PHIrs*rmT-PHIss*x[1]])-Bs*(dBs-sqrt[phi0s-PHIsf*qfhat+PHIrs*rmT-PHIss*x[1]]))+(1-gs)*(p0s-(Bs*PHIss*x[1])/(2*sqrt[phi0s+PHIrs*rmT-PHIss*x[1]-PHIsf*x[2]])-Bs*(dBs-sqrt[phi0s+PHIrs*rmT-PHIss*x[1]-PHIsf*x[2]]))
+    D2<-gf*(p0f-(Bf*PHIff*x[2])/(2*sqrt[phi0f-PHIfs*qshat+PHIrf*rmT-PHIff*x[2]])-Bf*(dBf-sqrt[phi0f-PHIfs*qshat+PHIrf*rmT-PHIff*x[2]]))+(1-gf)*(p0f-(Bf*PHIff*x[2])/(2*sqrt[phi0f+PHIrf*rmT-PHIfs*x[1]-PHIff*x[2]])-Bf*(dBf-sqrt[phi0f+PHIrf*rmT-PHIfs*x[1]-PHIff*x[2]]))
+    return(list(D1=D1, D2=D2))
+  })
+  q_double <- rootSolve::multiroot(f=cheat_equations,start=c(params$Qs*0.9,params$Qf*0.9))
+  return(q_double)
+}
+
+
+# z constraints
+unconA_zMinSwiss <- function(params,q_vals) {
+  with(q_vals, # q_vals should include qsstar,qfstar,qshat,qfhat,qsdouble,qfdouble
+       with(params,
+            es+p0s*(Qs-qshat)-p0s*(Qs-qsstar)-crs*rmN-Bs*qsstar*(dBs-sqrt[h0s^2-PHIsf*qfstar-PHIss*qsstar+PHIrs*rmN])+crs*rmT+Bs*qshat*(dBs-sqrt[h0s^2-PHIsf*qfdouble-PHIss*qshat+PHIrs*rmT])-gs*(Bs*qshat*(dBs-sqrt[h0s^2-PHIsf*qfdouble-PHIss*qshat+PHIrs*rmT])-Bs*qshat*(dBs-sqrt[h0s^2-PHIsf*qfhat-PHIss*qshat+PHIrs*rmT]))
+       ))}
+unconA_zMaxFrench <- function(params,q_vals) {
+  with(q_vals,
+       with(params,
+            -ef-p0f*(Qf-qfhat)+p0f*(Qf-qfstar)+Bf*qfstar*(dBf-sqrt[h0f^2-PHIff*qfstar-PHIfs*qsstar+PHIrf*rmN])-Bf*qfhat*(dBf-sqrt[h0f^2-PHIff*qfhat-PHIfs*qsdouble+PHIrf*rmT])+gf*(Bf*qfhat*(dBf-sqrt[h0f^2-PHIff*qfhat-PHIfs*qsdouble+PHIrf*rmT])-Bf*qfhat*(dBf-sqrt[h0f^2-PHIff*qfhat-PHIfs*qshat+PHIrf*rmT]))
+       ))}
+unconA_zRange <- function(params,q_vals) {
+  zRange <- unconA_zMaxFrench(params,q_vals) - unconA_zMinSwiss(params,q_vals)
+  return(zRange)
+}
