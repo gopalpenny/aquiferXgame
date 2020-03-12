0ddb02f6898ee687d90d32bf46b8948b357fb256
Gopal <gopalpenny@gmail.com>
2020-03-11 22:54:43 -0400

    added evaluate_treaty_cases test

--- tests/testthat/test-game-evaluate.R
+++ tests/testthat/test-game-evaluate.R
@@ -1,5 +1,6 @@
 # test_game_evaluate.R
 
+## evaluate_treaty
 test_that("evaluate_agreement returns tibble",{
   expect_equal(class(evaluate_treaty(example_params_confined)),c("tbl_df","tbl","data.frame"))
 })
@@ -15,13 +16,25 @@ df <- tibble::tibble(treaty="Y",
                  qsdouble=9.0375939849624,
                  qfdouble=9.0375939849624) %>%
   dplyr::mutate_if(is.numeric,function(x) round(x,digits=4))
-test_that("evaluate_agreement returns correct zRange, q values for confined aquifer",{
+test_that("evaluate_treaty returns correct zRange, q values for confined aquifer",{
   expect_equal(evaluate_treaty(example_params_confined) %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)),
                df)
 })
 
 evaluate_treaty(example_params_unconfined)
-test_that("evaluate_agreement returns correct zRange, q values for unconfined aquifer",{
+test_that("evaluate_treaty returns correct zRange, q values for unconfined aquifer",{
   expect_equal(evaluate_treaty(example_params_unconfined) %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)),
                df)
+})
+
+
+## evaluate_treaty_cases
+df_cases <- tibble::tibble(treaty="Y", zRange=0.0443,zMinSwiss=-0.0222,zMaxFrench=0.0222,
+                           qshat=6.8686, qfhat=6.8686, qsstar=8.7418, qfstar=8.7418, qsdouble=9.0376, qfdouble=9.0376,
+                           Us_hat=-17.4976, Uf_hat=-16.6976,Us_star=-17.7432,Uf_star=-16.9432,Us_double=-17.2388, Uf_double=-16.4388,Us_hat_double=-17.9445,Uf_hat_double=-17.1445,
+                           ds_hat=25.96,df_hat=25.96,ds_star=32.5164,df_star=32.5164,ds_double=30.298, df_double=30.298,ds_hat_double=30.298,df_hat_double=30.298) %>%
+  dplyr::mutate_if(is.numeric,function(x) round(x,digits=4))
+test_that("evaluate_treaty_cases returns correct zRange, q, U, and d values",{
+  expect_equal(evaluate_treaty_cases(example_params_confined,"qud") %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)),
+               df_cases)
 })
