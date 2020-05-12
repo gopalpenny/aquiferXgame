f57f8d2c0752d76e8d70a195b2f4a349ce5bf3f6
Gopal Penny <gopalpenny@gmail.com>
2020-05-12 09:01:13 -0400

    Updated tests to account for xi_double_double

--- tests/testthat/test-game-evaluate.R
+++ tests/testthat/test-game-evaluate.R
@@ -23,7 +23,7 @@ test_that("evaluate_treaty returns correct zRange, q values for confined aquifer
 
 
 ## evaluate_treaty_cases
-df_cases <- tibble::tibble(treaty="Y", zRange=0.0443, zMinSwiss=-0.0222, zMaxFrench=0.0222, qshat=6.8686, qsstar=8.7418, qsdouble=9.0376, qfhat=6.8686, qfstar=8.7418, qfdouble=9.0376, Us_hat=-17.4976, Us_star=-17.7432, Us_double=-17.2388, Us_hat_double=-17.9445, Uf_hat=-16.6976, Uf_star=-16.9432, Uf_double=-16.4388, Uf_hat_double=-17.1445, ds_hat=25.96, ds_star=32.5164, ds_double=30.298, ds_hat_double=29.2135, df_hat=25.96, df_star=32.5164, df_double=30.298, df_hat_double=29.2135)
+df_cases <- tibble::tibble(treaty="Y", zRange=0.0443, zMinSwiss=-0.0222, zMaxFrench=0.0222, qshat=6.8686, qsstar=8.7418, qsdouble=9.0376, qfhat=6.8686, qfstar=8.7418, qfdouble=9.0376, Us_hat=-17.4976, Us_star=-17.7432, Us_double=-17.2388, Us_hat_double=-17.9445, Us_double_double=-17.8269, Uf_hat=-16.6976, Uf_star=-16.9432, Uf_double=-16.4388, Uf_hat_double=-17.1445, Uf_double_double=-17.0269, ds_hat=25.96, ds_star=32.5164, ds_double=30.298, ds_hat_double=29.2135, ds_double_double=33.5516, df_hat=25.96, df_star=32.5164, df_double=30.298, df_hat_double=29.2135, df_double_double=33.5516)
 # evaluate_treaty_cases(example_params_confined,"qud") %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)) %>% ggp::print_data_frame_for_entry(single_line = T)
 test_that("evaluate_treaty_cases returns correct zRange, q, U, and d values",{
   expect_equal(evaluate_treaty_cases(example_params_confined,"qud") %>% dplyr::mutate_if(is.numeric,function(x) round(x,digits=4)),
@@ -38,9 +38,9 @@ test_that("evaluate_treaty_cases CONFINED returns all input parameters for optio
   expect_equal(evaluate_treaty_cases(example_params_confined,"q") %>% names(),
                c('treaty','zRange','zMinSwiss','zMaxFrench','qshat','qsstar','qsdouble','qfhat','qfstar','qfdouble'))
   expect_equal(evaluate_treaty_cases(example_params_confined,"u") %>% names(),
-               c('treaty','zRange','zMinSwiss','zMaxFrench','Us_hat','Us_star','Us_double','Us_hat_double','Uf_hat','Uf_star','Uf_double','Uf_hat_double'))
+               c('treaty','zRange','zMinSwiss','zMaxFrench','Us_hat','Us_star','Us_double','Us_hat_double','Us_double_double','Uf_hat','Uf_star','Uf_double','Uf_hat_double','Uf_double_double'))
   expect_equal(evaluate_treaty_cases(example_params_confined,"d") %>% names(),
-               c('treaty','zRange','zMinSwiss','zMaxFrench','ds_hat','ds_star','ds_double','ds_hat_double','df_hat','df_star','df_double','df_hat_double'))
+               c('treaty','zRange','zMinSwiss','zMaxFrench','ds_hat','ds_star','ds_double','ds_hat_double','ds_double_double','df_hat','df_star','df_double','df_hat_double','df_double_double'))
 })
 
 df_grid <- expand.grid(x=seq(-5,5,length.out=20),y=seq(-5,5,length.out=20))
@@ -67,8 +67,8 @@ params <- tidyr::crossing(params,gs=seq(0,1,by=0.05))
 treaty_df <- evaluate_treaty_cases(params,'quda')
 # results <- gather_outcomes(treaty_df)
 test_that('gather_outcomes working',{
-  expect_equal(dim(gather_outcomes(treaty_df)),c(462,34))
-  expect_equal(dim(gather_outcomes(treaty_df,expectation=TRUE)),c(546,34))
+  expect_equal(dim(gather_outcomes(treaty_df)),c(546,34))
+  expect_equal(dim(gather_outcomes(treaty_df,expectation=TRUE)),c(630,34))
 })
 
 
--- tests/testthat/test-game-unconfined-linear.R
+++ tests/testthat/test-game-unconfined-linear.R
@@ -31,9 +31,9 @@ test_that("evaluate_treaty_cases UNCONFINED returns all input parameters for opt
   expect_equal(results_q %>% names(),
                c('treaty','zRange','zMinSwiss','zMaxFrench','qshat','qsstar','qsdouble','qfhat','qfstar','qfdouble'))
   expect_equal(results_u %>% names(),
-               c('treaty','zRange','zMinSwiss','zMaxFrench','Us_hat','Us_star','Us_double','Us_hat_double','Uf_hat','Uf_star','Uf_double','Uf_hat_double'))
+               c('treaty','zRange','zMinSwiss','zMaxFrench','Us_hat','Us_star','Us_double','Us_hat_double','Us_double_double','Uf_hat','Uf_star','Uf_double','Uf_hat_double','Uf_double_double'))
   expect_equal(results_d %>% names(),
-               c('treaty','zRange','zMinSwiss','zMaxFrench','ds_hat','ds_star','ds_double','ds_hat_double','df_hat','df_star','df_double','df_hat_double'))
+               c('treaty','zRange','zMinSwiss','zMaxFrench','ds_hat','ds_star','ds_double','ds_hat_double','ds_double_double','df_hat','df_star','df_double','df_hat_double','df_double_double'))
 })
 
 
@@ -43,8 +43,9 @@ test_that("evaluate_treaty_cases UNCONFINED returns all input parameters for opt
 # uconfined aquifers:
 # first estimate of first best produces negative qs, maximum qf=5. second estimate must have qs>0
 df_input <- tibble::tibble(B=0.91, dBf=75.4, dBs=75.4, h0f=45.3, h0s=42.7, p0s=100, PHIff=110, PHIfrN=97.5, PHIfrT=97.5, PHIfs=91.4, PHIsf=91.4, PHIsrN=121, PHIsrT=121, PHIss=110, year=1989, row=50, Qs=15, Qf=5, gs=1, gf=1, p0f=5e+05, crs=1e+05, rmN=8.2, rmT=8.2, es=0, ef=0, l=1)
-df_output <- tibble::tibble(treaty="N", zRange=0, zMinSwiss=0, zMaxFrench=0, qshat=15, qsstar=15, qsdouble=15, qfhat=5, qfstar=5, qfdouble=5, ds_hat=48.7825, ds_star=48.7825, ds_double=48.7825, ds_hat_double=48.7825, df_hat=44.8944, df_star=44.8944, df_double=44.8944, df_hat_double=44.8944)
+df_output <- tibble::tibble(treaty="N", zRange=0, zMinSwiss=0, zMaxFrench=0, qshat=15, qsstar=15, qsdouble=15, qfhat=5, qfstar=5, qfdouble=5, ds_hat=48.7825, ds_star=48.7825, ds_double=48.7825, ds_hat_double=48.7825, ds_double_double=48.7825, df_hat=44.8944, df_star=44.8944, df_double=44.8944, df_hat_double=44.8944, df_double_double=44.8944)
 df_results <- suppressWarnings(evaluate_treaty_cases(df_input,'qd'))
+# df_results %>% mutate_if(is.numeric,function(x) round(x,4)) %>% ggp::print_data_frame_for_entry(single_line = TRUE)
 test_that("evaluate_treaty_cases first estimate of first best produces negative qs, maximum qf=5. second estimate must have qs>0",{
   expect_equal(df_results %>% tidyr::gather(var,val,-treaty) %>% dplyr::mutate(val=round(val,4)),df_output %>% tidyr::gather(var,val,-treaty))
 })
@@ -67,9 +68,10 @@ test_that('evaluate_treaty_cases returns \'D\' for depleted aquifer',{
 # in row 2, qfhat is 5, qfdouble should be 5
 df_input_qdouble <- data.frame(B=c(0.91, 0.91), dBf=c(75.4, 75.4), dBs=c(75.4, 75.4), h0f=c(45.3, 45.3), h0s=c(42.7, 42.7), p0s=c(1e+05, 1e+05), year=c(1956, 1976), row=c(17, 37), Qs=c(17, 20), Qf=c(0, 5), gs=c(0.6, 0.925), gf=c(0.6, 0.925), p0f=c(1000, 200), crs=c(50000, 10000), rmN=c(0, 8.2), rmT=c(0, 8.2), es=c(0, 0), ef=c(0, 0), drawdown_years=c(5, 50),
                                PHIff=c(34.9166483561281, 109.80662752661), PHIfrT=c(18.6388805781934, 97.4085453273349), PHIfs=c(14.362429420851, 91.3701219010559), PHIsf=c(14.3624294208507, 91.3701219010546), PHIsrT=c(29.6525338473577, 120.883872795857), PHIss=c(25.9900272148918, 110.030353031565), PHIsrN=c(59.3050676947154, 241.767745591714), PHIfrN=c(9.3194402890967, 48.7042726636675), l = c(1,1))
-df_output_qdouble <- tibble::tibble(treaty=c("N", "N"), zRange=c(0, -328.9488), zMinSwiss=c(0, 388.6396), zMaxFrench=c(0, 59.6909), qshat=c(17, 20), qsstar=c(17, 20), qsdouble=c(17, 20), qfhat=c(0, 5), qfstar=c(0, 5), qfdouble=c(0, 5), ds_hat=c(38.232, 62.8668), ds_star=c(38.232, 41.513), ds_double=c(38.232, 62.8668), ds_hat_double=c(38.232, 62.8668), df_hat=c(32.8803, 53.6192), df_star=c(32.8803, 66.738), df_double=c(32.8803, 53.6192), df_hat_double=c(32.8803, 53.6192))
-df_qdoublt_results <- suppressWarnings(evaluate_treaty_cases(df_input_qdouble,'qd'))
+df_output_qdouble <- tibble::tibble(treaty=c("N", "N"), zRange=c(0, -328.9488), zMinSwiss=c(0, 388.6396), zMaxFrench=c(0, 59.6909), qshat=c(17, 20), qsstar=c(17, 20), qsdouble=c(17, 20), qfhat=c(0, 5), qfstar=c(0, 5), qfdouble=c(0, 5), ds_hat=c(38.232, 62.8668), ds_star=c(38.232, 41.513), ds_double=c(38.232, 62.8668), ds_hat_double=c(38.232, 62.8668), ds_double_double=c(38.232, 62.8668), df_hat=c(32.8803, 53.6192), df_star=c(32.8803, 66.738), df_double=c(32.8803, 53.6192), df_hat_double=c(32.8803, 53.6192), df_double_double=c(32.8803, 53.6192))
+df_qdouble_results <- suppressWarnings(evaluate_treaty_cases(df_input_qdouble,'qd'))
+# df_qdouble_results %>% mutate_if(is.numeric,function(x) round(x,4)) %>% ggp::print_data_frame_for_entry(single_line = TRUE)
 test_that('evaluate_treaty_cases returns qx_double equal to qi_hat and qi_star in cases where qj is 0',{
-  expect_equal(df_qdoublt_results %>% dplyr::mutate_if(is.numeric,function(x) round(x,4)),
+  expect_equal(df_qdouble_results %>% dplyr::mutate_if(is.numeric,function(x) round(x,4)),
                df_output_qdouble)
 })
